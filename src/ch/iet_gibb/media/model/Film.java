cd852aa3415df1a32262641e9d4b70df3822c55a
Marjan Tomev <mto150238@stud.gibb.ch>
2024-09-19 08:56:38 +0200

    added comments

--- src/ch/iet_gibb/media/model/Film.java
+++ src/ch/iet_gibb/media/model/Film.java
@@ -2,10 +2,28 @@ package ch.iet_gibb.media.model;
 
 import java.util.List;
 
+/**
+ * Represents a film.
+ *
+ * @author Marjan Tomev
+ * @version 1.0
+ * @since 1.0
+ */
 public class Film extends Media {
     protected List<String> actors;
     protected String duration;
 
+    /**
+     * Creates a new film.
+     *
+     * @param title the title of the film
+     * @param year the year of the film
+     * @param genres the genres of the film
+     * @param creators the creators of the film
+     * @param available the availability of the film
+     * @param actors the actors of the film
+     * @param duration the duration of the film
+     */
     public Film(String title,
                 String year,
                 List<String> genres,
@@ -18,10 +36,16 @@ public class Film extends Media {
         this.duration = duration;
     }
 
+    /**
+     * Returns the duration of the film.
+     * */
     public List<String> getActors() {
         return actors;
     }
 
+    /**
+     * Sets the duration of the film.
+     * */
     public void setActors(List<String> actors) {
         this.actors = actors;
     }
